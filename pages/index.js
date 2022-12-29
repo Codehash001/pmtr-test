b4f32128d23ae3ac25e273f789635f983c8dff07
Codehash001 <codehashblockchain@gmail.com>
2022-12-29 21:05:43 +0530

    rebuild

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,72 @@ function home () {
     bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border-2 border-gray-100 backdrop-saturate-150">
         <div className="flex flex-col items-center">
           <h1 className="font-Kanit text-[35px] text-white uppercase">Minting is Live!</h1>
-          <h1 className='font-Kanit text-[25px] text-gray-400 mt-2'>Not Connected</h1>
+          <h1 className='font-Kanit text-[25px] text-gray-400'>Not Connected</h1>
+          <div className="flex flex-col items-center w-full px-4 mt-16 md:mt-0 ">
+            {/* Increment decrement buttons */}
+                <div className="font-Kanit flex items-center justify-between w-full">
+                  <button
+                    className="w-12 h-8 md:w-14 md:h-10 flex items-center justify-center text-black hover:shadow-lg bg-gray-300 font-bold rounded-md"
+                    onClick=''>
+                   
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 md:h-8 md:w-8"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                      />
+                    </svg>
+                  </button>
+                  <p className="flex items-center justify-center flex-1 grow text-center font-bold text-brand-02 text-3xl md:text-4xl">
+                  {mintAmount}
+                  </p>
+                  <button
+                    className="w-12 h-8 md:w-14 md:h-10 flex items-center justify-center text-black hover:shadow-lg bg-gray-300 font-bold rounded-md"
+                    onClick='' >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 md:h-8 md:w-8"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M18 12H6"
+                      />
+                    </svg>
+                  </button> 
+                </div>  
+                <p className="text-sm text-gray-100 tracking-widest mt-5">
+                  Max Mint Amount Per Wallet: 10
+                  {/* {paused ? '0' : isWlMint ? config.WlMaxMintAmount : config.maxMintAmount} */}
+                </p>
+
+                <div className="border-t border-b py-4 mt-9 w-full">
+                  <div className="w-full text-xl font-Kanit flex items-center justify-between text-yellow-300">
+                    <p>Total</p>
+
+                    <div className="flex items-center space-x-3">
+                    <p>
+                        {/* {Number.parseFloat(paused ? '0.00' : isWlMint && EligbleForFreeMint ? config.wlcost*(mintAmount-1) : isWlMint ? config.wlcost*mintAmount : config.publicSalePrice*mintAmount).toFixed(
+                          2
+                        )}{' '} */}
+                        0.001{''}ETH
+                      </p>{' '}
+                      <span className="text-yellow-300">+ GAS</span>
+                    </div>
+                  </div>
+                </div>
+                </div>
         </div>
       </div>
 
