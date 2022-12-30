54192acecb084426ac118cc12a7fc66c4bcc6f9d
Codehash001 <codehashblockchain@gmail.com>
2022-12-30 23:40:39 +0530

    rebuild

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,36 +1,40 @@
-import { useState } from "react";
+import React, { Component } from "react";
+import Slider from "react-slick";
 
-function Carousel() {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  // Replace with your own images
-  const images = [
-    "/1.png",
-    "/2.png",
-    "/3.png",
-    "/4.png",
-    
-  ];
-
-  // Set the interval for automatically changing the image
-  setInterval(() => {
-    setCurrentImageIndex((currentImageIndex + 1) % images.length);
-  }, 2000);
-
-  return (
-    <div className="relative h-64 w-full">
-      <div
-        className="absolute inset-0 h-full w-full bg-gray-600 bg-opacity-25"
-      ></div>
-
-      <div className="relative h-full w-full flex items-center justify-center">
-        <img
-          src={images[currentImageIndex]}
-          className="h-full w-full object-cover"
-          alt=""
-        />
+export default class Slider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 2000,
+      autoplaySpeed: 2000,
+      cssEase: "linear"
+    };
+    return (
+      <div>
+        <h2>Auto Play</h2>
+        <Slider {...settings}>
+          <div className="mx-2">
+            <img src='1.png' className="w-[100px] h-[100px]"/>
+          </div>
+          <div className="mx-2">
+            <img src='2.png' className="w-[100px] h-[100px]"/>
+          </div>
+          <div className="mx-2">
+            <img src='3.png' className="w-[100px] h-[100px]"/>
+          </div>
+          <div className="mx-2">
+            <img src='4.png' className="w-[100px] h-[100px]"/>
+          </div>
+          <div className="mx-2">
+            <img src='5.png' className="w-[100px] h-[100px]"/>
+          </div>
+         
+        </Slider>
       </div>
-    </div>
-  );
-};
-export default Carousel
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,10 @@
 import { Rerousel } from 'rerousel';
 import { useState, useEffect, useRef } from "react";
+import Slider from '../components/Carousel';
 
 
 function home () {
 
-  const ref = useRef(null);
-
-  const Item = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: calc(100%/2);
-    height: 100px;
-    font-family: Kanit;
-    font-weight: bold;
-    font-size: 1.5em;
-    border: solid 1px black;
-    background-color: #61DAFB;
-    
-    @media(max-width: 1150px) {
-        width: 100%
-    }
-  `;
 
 
   return (
@@ -132,13 +115,7 @@ function home () {
       </div>
 
       <div className='relative'>
-        <Rerousel itemRef={ref}>
-        <Item ref={ref}>1</Item>
-        <Item>2</Item>
-        <Item>3</Item>
-        <Item>4</Item>
-        <Item>5</Item>
-        </Rerousel>
+        <Slider/>
       </div>
 
     </div>
