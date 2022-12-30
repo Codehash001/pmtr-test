2418f9774bc57808420a345cc1d09f42ebceedfc
Codehash001 <codehashblockchain@gmail.com>
2022-12-31 03:08:18 +0530

    css fix

--- pages/index.js
+++ pages/index.js
@@ -96,13 +96,13 @@ function home () {
                 
                     {/* mint button */}
 
-                    <a href="/" class="relative mt-5 px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group font-Kanit">
+                    <a href="/" class="relative mt-5 px-5 py-3 overflow-hidden font-medium text-gray-600 bg-bg-gradient-to-r from-amber-300 via-orange-400 to-red-500 border border-gray-100 rounded-lg shadow-inner group font-Kanit">
 <span class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
 <span class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
-<span class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-<span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-<span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-<span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Connect Wallet</span>
+<span class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-400 group-hover:h-full ease"></span>
+<span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-400 group-hover:h-full ease"></span>
+<span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-100 opacity-0 group-hover:opacity-100"></span>
+<span class="relative transition-colors duration-300 delay-200 group-hover:bg-gradient-to-r from-amber-300 via-orange-400 to-red-500 bg-clip-text text-transparent ease">Connect Wallet</span>
 </a>
 
 {/* social media icons */}
