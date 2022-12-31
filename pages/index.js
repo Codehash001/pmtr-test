4a22bcd952e92aaf4738462e44121246f9b90a6d
Codehash001 <codehashblockchain@gmail.com>
2022-12-31 13:23:50 +0530

    css fix

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ function home () {
             </h3>
 
             <p className="text-2xl text-white font-semibold font-Kanit mt-5 tracking-wide">
-                  1/1000 NFts minted so far
+                  1/1000 NFTs minted
             </p>
                   
             {/* Increment decrement buttons */}
@@ -97,9 +97,9 @@ function home () {
                     {/* mint button */}
 
                     <a href="#_" class="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-Kanit font-medium tracking-tighter text-white bg-gray-800 rounded-lg group mt-5">
-<span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+<span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-linear-gradient(to right, rgb(202, 138, 4), rgb(220, 38, 38)) rounded-full group-hover:w-56 group-hover:h-56"></span>
 <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-<span class="relative">Connect Wallet</span>
+<span class="relative tracking-wide font-mediumr">Connect Wallet</span>
 </a>
 
 {/* social media icons */}
